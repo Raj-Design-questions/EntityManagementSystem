26499cc2f77a217eb01cb545fb54475f52c2d7d2
Rahul Chauhan <rahulchauhanraj@gmail.com>
2018-01-30 21:28:48 +0530

    Add files via upload

---
+++ src/main/java/com/rah/target/ems/boot/Application.java
@@ -0,0 +1,27 @@
+package com.rah.target.ems.boot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(
+    value = {
+        "com.rah.target.ems"
+    },
+    excludeFilters = {
+    }
+)
+public class Application extends SpringBootServletInitializer {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).run(args);
+        log.info("Application starting...................");
+        log.info("Application started....................");
+    }
+}
